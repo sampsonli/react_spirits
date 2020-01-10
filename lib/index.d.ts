05bed358a40367dcc47fe23df63e906a733662c2
lichun <lichun@mistong.com>
2020-01-10 09:36:00 +0800

    Revert "optimate"
    This reverts commit 9aee3e9e

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,30 @@
-export declare function connect<N>(ns: N): Function;
-export declare function action(clazz: any, act: any): any;
+interface Context {
+    commit: (mName: string, payload?: any) => any;
+    dispatch: (actionType: string, payload?: any) => any;
+    state: object;
+    rootState: object;
+}
+interface Act {
+    [fname: string]: (this: Context | any, payload?: any, context?: Context) => any;
+}
+interface Mt<S> {
+    [fname: string]: (this: S, payload?: any, state?: S) => any;
+}
+declare type OAct<A> = {
+    [act in keyof A]: (payload?: any) => any;
+};
+declare type OMt<M> = {
+    [mt in keyof M]: (payload?: any) => any;
+};
+export declare function connect<S extends object, M extends Mt<S>, A extends Act, N extends string>(model: {
+    ns: N;
+    act: A;
+    mt: M;
+    state: S;
+}): {
+    act: OAct<A>;
+    mt: OMt<M>;
+    ns: N;
+};
 declare const _default: (store: any, asyncReducers?: {}) => void;
 export default _default;
